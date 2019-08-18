7ecbf1f34421e43cc1f287a7c2fe38a92f83a98d
Smolyakov <mompizzapls@mail.ru>
2019-08-19 00:39:43 +0300

    First working unit test

--- movie.test/Acceptance/MovieControllerTest.cs
+++ movie.test/Acceptance/MovieControllerTest.cs
@@ -1,33 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
-using movie.test.Utils;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using RazorPagesMovie.Models;
 
 namespace movie.test.Acceptance
 {
-
     [TestFixture]
     public class MovieControllerTest
     {
         private WebApplicationFactory<RazorPagesMovie.Startup> _factory;
+        private HttpClient _client;
 
         [OneTimeSetUp]
         public void SetUp()
         {
-            _factory = new ApiWebApplicationFactory();
+            _factory = new WebApplicationFactory<RazorPagesMovie.Startup>();
+            _client = _factory.CreateClient();
         }
 
         [Test]
         public async Task GET_GetItems()
         {
-            var client = _factory.CreateClient();
-
-            var response = await client.GetAsync("/api/movie");
+            var response = await _client.GetAsync("/api/movie");
 
             response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
-//            var t = response.Content.ToString();
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<IEnumerable<Movie>>(stringResult).ToList();
+            Assert.That(movies, Has.Count.EqualTo(4));
+
+            var firstMovie = movies.First();
+            Assert.AreEqual(firstMovie.Id, 1);
         }
 
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
     }
 }
--- movie.test/Utils/ApiWebApplicationFactory.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-
-namespace movie.test.Utils
-{
-    public class ApiWebApplicationFactory : WebApplicationFactory<RazorPagesMovie.Startup>
-    {
-    }
-}
