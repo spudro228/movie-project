1da965918892d5c25157e5f23cc3f5fdb7c82ab6
Smolyakov <mompizzapls@mail.ru>
2019-08-19 00:55:30 +0300

    test for get item by id

--- movie.test/Acceptance/MovieControllerTest.cs
+++ movie.test/Acceptance/MovieControllerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -37,6 +38,25 @@ namespace movie.test.Acceptance
 
             var firstMovie = movies.First();
             Assert.AreEqual(firstMovie.Id, 1);
+        }
+        
+        [Test]
+        public async Task GET_GetItemById()
+        {
+            var response = await _client.GetAsync("/api/movie/1");
+
+            response.EnsureSuccessStatusCode();
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<Movie>(stringResult);
+
+            Assert.AreEqual(movies.Id, 1);
+            Assert.AreEqual(movies.Genre, "Romantic Comedy");
+            Assert.AreEqual(movies.Price, new decimal(7.99));
+            Assert.AreEqual(movies.Rating, "R");
+            Assert.AreEqual(movies.ReleaseDate, new DateTime(1989,2,12));
+            Assert.AreEqual(movies.Title, "When Harry Met Sally");
         }
 
         [OneTimeTearDown]
